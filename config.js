59161320438609b79ae03770291f026d780e71fa
yvizbulatov-ops <yv.izbulatov@gmail.com>
2026-07-29 21:36:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const  SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw8BeMM8fBotozxjyNIhC5UoI-LY-0mNkeFDInASAZ5/exec
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw8BeMM8fBotozxjyNIhC5UoI-LY-0mNkeFDInASAZ5/exec';
